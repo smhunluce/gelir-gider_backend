83d6a0da710a85cfefd0427ab2b5ea3a18fa38bd
unluce@itu.edu.tr <semih0326esk123>
2022-02-27 18:08:20 +0300

    new route for transaction history & give name to unnamed routes

--- routes/api.php
+++ routes/api.php
@@ -16,14 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::group([
     'prefix' => 'auth',
 ], function () {
-    Route::post('login', 'App\Http\Controllers\AuthController@login');
-    Route::post('register', 'App\Http\Controllers\AuthController@register');
+    Route::post('login', 'App\Http\Controllers\AuthController@login')->name('auth.login');
+    Route::post('register', 'App\Http\Controllers\AuthController@register')->name('auth.register');
 
     Route::group([
         'middleware' => 'auth:api',
     ], function () {
-        Route::get('logout', 'App\Http\Controllers\AuthController@logout');
-        Route::get('user', 'App\Http\Controllers\AuthController@user');
+        Route::get('logout', 'App\Http\Controllers\AuthController@logout')->name('auth.logout');
+        Route::get('user', 'App\Http\Controllers\AuthController@user')->name('auth.user');
     });
 });
 
@@ -32,8 +32,14 @@ Route::apiResource(
     'App\Http\Controllers\TransactionCategoryController',
     ['except' => ['update', 'destroy']]
 )->middleware('auth:api');
+
 Route::apiResource(
     'transaction',
     'App\Http\Controllers\TransactionController'
 )->middleware('auth:api');
+
+Route::get(
+    'transaction/history',
+    'App\Http\Controllers\TransactionController@history'
+)->name('transaction.history')->middleware('auth:api');
 
